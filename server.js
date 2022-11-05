6230255282fb3cf36507cf659b13e541232d1760
zachpaul77 <zachpaul121@gmail.com>
2022-11-04 21:08:04 -0400

    cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 
 // middleware
 app.use(express.json())
-app.use(cors(/*{origin: "https://schedge.netlify.app"}*/))
+app.use(cors({origin: "https://schedge.netlify.app", credentials: true}))
 
 // routes
 app.use('/api/room', roomRoutes)
